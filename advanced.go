a8ba21b1af0d5242fcdc33e6342c4d5886c13ea0
nairo <nairobi93@mail.ru>
2020-12-07 15:51:01 +0300

    I made advanced task

---
+++ advanced.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var n string
+	var in, i int64
+	var primeArr []int64
+
+	fmt.Print("Введите  число N: ")
+	fmt.Scanln(&n)
+	in, err := strconv.ParseInt(n, 0, 64)
+	if err != nil {
+		fmt.Println("Вы ввели не число!")
+		os.Exit(1)
+	}
+
+	for i = 0; i <= in; i++ {
+		if isSimple(i) {
+			primeArr = append(primeArr, i)
+		}
+	}
+
+	fmt.Println(primeArr)
+
+}
+
+func isSimple(num int64) bool {
+	var i int64
+	var sqrtNum = int64(math.Sqrt(float64(num)))
+	for i = 2; i < sqrtNum; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
